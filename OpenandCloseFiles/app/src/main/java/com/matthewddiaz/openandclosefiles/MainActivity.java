7ccfb87cf26ebad163881f629c6e0132b84ce17c
Matthew <matthewdiaz10@ufl.edu>
2015-03-03 03:21:01 -0500

    Add another Thread which sleeps until the CounterThread is done and then displays the File text!

--- OpenandCloseFiles/app/src/main/java/com/matthewddiaz/openandclosefiles/MainActivity.java
+++ OpenandCloseFiles/app/src/main/java/com/matthewddiaz/openandclosefiles/MainActivity.java
@@ -22,7 +22,8 @@ import java.io.OutputStreamWriter;
 public class MainActivity extends Activity {
     private TextView mText;
     private EditText mUserInput;
-    private CounterThread mThread;
+    private CounterThread mCounterThread;
+    private MakingFileThread mFileThread;
 
 
     @Override
@@ -31,40 +32,26 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         mText = (TextView)findViewById(R.id.text);
         mUserInput = (EditText)findViewById(R.id.userInput);
-        startThread();
     }
 
-    public void startThread(){
-        mThread = new CounterThread();
-        mThread.start();
+    public void startCounterThread(){
+        mCounterThread = new CounterThread();
+        mCounterThread.start();
+    }
+
+    public void startMakingFileThread(String s1,String s2){
+        mFileThread = new MakingFileThread(s1,s2);
+        mFileThread.start();
     }
 
     public void button_handler(View v){
         String val = mUserInput.getText().toString();
+        startCounterThread();
         String file = "myfile.txt";
-        try {
-            FileOutputStream outputStream = openFileOutput(file,MODE_PRIVATE);
-            BufferedWriter writer = new BufferedWriter((new OutputStreamWriter(outputStream,"UTF-8")));
-            writer.write(val);
-            writer.close();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        try {
-            FileInputStream fis = openFileInput( file );
-            BufferedReader reader = new BufferedReader((new InputStreamReader(fis,"UTF-8")));
-            String ans = reader.readLine();
-            mText.setText(ans);
-            reader.close();
+        startMakingFileThread(val,file);
+       }
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void updateAnswers(final long seconds){
+    public void updateSeconds(final long seconds){
         Runnable UIdoWork = new Runnable(){/*need to but anything that changes view make UI thread work.
             since the Worker thread called updateAnswers with RunOnUIThread, the worker
             thread would be the one executing the code mText.setText(ans) which causes the
@@ -73,6 +60,16 @@ public class MainActivity extends Activity {
             public void run(){
                 String ans = String.valueOf(seconds);
                 mText.setText(ans);
+            }
+        };
+        runOnUiThread(UIdoWork);
+    }
+
+
+    public void updateWriteFile(final String userText){
+        Runnable UIdoWork = new Runnable(){
+            public void run(){
+                mText.setText(userText);
             }
         };
         runOnUiThread(UIdoWork);
@@ -110,8 +107,50 @@ public class MainActivity extends Activity {
                 }catch(InterruptedException e){
                     e.printStackTrace();
                 }
-                updateAnswers(++count);
+                updateSeconds(++count);
             }
+        }
+    }
+
+    private class MakingFileThread extends Thread{
+        String userInput;
+        String fileName;
+
+        public MakingFileThread(String s1,String s2){
+            userInput = s1;
+            fileName = s2;
+        }
+
+        @Override
+        public void run(){
+
+            try{
+                sleep(11000);
+            }
+            catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                FileOutputStream outputStream = openFileOutput(fileName,MODE_PRIVATE);
+                BufferedWriter writer = new BufferedWriter((new OutputStreamWriter(outputStream,"UTF-8")));
+                writer.write(userInput);
+                writer.close();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            try {
+                FileInputStream fis = openFileInput( fileName );
+                BufferedReader reader = new BufferedReader((new InputStreamReader(fis,"UTF-8")));
+                String ans = reader.readLine();
+                updateWriteFile(ans);
+                reader.close();
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
