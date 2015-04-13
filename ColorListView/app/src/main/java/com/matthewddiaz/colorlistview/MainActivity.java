3de6a51af77ebf79db442fb19110d8ab679d06a9
Matthew <matthewdiaz10@ufl.edu>
2015-04-13 14:24:23 -0400

    Add setOnItemClickListener allows for touching an individual view in the ListView to display a Toast.

--- ColorListView/app/src/main/java/com/matthewddiaz/colorlistview/MainActivity.java
+++ ColorListView/app/src/main/java/com/matthewddiaz/colorlistview/MainActivity.java
@@ -4,23 +4,41 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list = (ListView)findViewById(R.id.List);
         populateList();
+        registerClickCallback();
     }
 
     public void populateList(){
         String[] colorList = {"blue","red","green","purple","white"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.color,colorList);
-        ListView list = (ListView)findViewById(R.id.List);
         list.setAdapter(adapter);
+    }
+
+    public void registerClickCallback(){
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView textView = (TextView)view;
+                String message = "The clicked #" + position + " and the color is "
+                                 + textView.getText().toString();
+                Toast.makeText(MainActivity.this,message,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
