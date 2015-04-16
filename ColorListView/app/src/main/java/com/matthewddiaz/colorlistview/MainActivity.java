d967c36eea66a9e28d37c171513b8d093aa92805
Matthew <matthewdiaz10@ufl.edu>
2015-04-16 02:28:18 -0400

    Update makingGradient to correctly make gradients!

--- ColorListView/app/src/main/java/com/matthewddiaz/colorlistview/MainActivity.java
+++ ColorListView/app/src/main/java/com/matthewddiaz/colorlistview/MainActivity.java
@@ -28,9 +28,11 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         mList = (ListView)findViewById(R.id.List);
         populateList();
+        registerClickCallback();
     }
 
-    public void makingGradients(float maxS,float leastS,int lVLength){
+    public void makingGradients(float minS,int lVLength){
+        float maxVal = 360.0f;//Saturation ranges from [0f - 360f]
         if(mDrawableList == null){
             mDrawableList = new ArrayList<GradientDrawable>();
         }
@@ -38,17 +40,17 @@ public class MainActivity extends ActionBarActivity {
         ColorMaker c1;
         ColorMaker c2;
 
-        float interval =  maxS - leastS;
-        float range = interval/lVLength;
-        float cSat = leastS;
+        float range = maxVal/lVLength;
+        float cSat = minS;
 
         GradientDrawable drawable;
         int leftColor;
         int rightColor;
         for(int i = 0; i != lVLength;++i){
-           float s1 = cSat;
-           cSat += range;
-           c1 = new ColorMaker(s1,1.0f,1.0f);
+           float leftSide = cSat%maxVal;//s1 is the value of the left side if min passes 360 then modulo!
+           cSat = (leftSide + range)%maxVal;//the right side is the left side + range. Just in case modulo 360!
+
+           c1 = new ColorMaker(leftSide,1.0f,1.0f);
            c2 = new ColorMaker(cSat,1.0f,1.0f);
 
            leftColor = c1.makeColor();
@@ -60,24 +62,21 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void populateList(){
-        makingGradients(345.0f,15.0f,11);
+        makingGradients(345.0f,12);
         if(mAdapter == null){
            mAdapter = new ColorAdapter(this,mDrawableList);
         }
         mList.setAdapter(mAdapter);
     }
 
-    /*
+
     public void registerClickCallback(){
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                TextView textView = (TextView)view;
-                view.setBackground(drawable);
-                String message = "The clicked #" + position + " and the color is "
-                                 + textView.getText().toString();
+                String message = "You clicked position" + position;
                 Toast.makeText(MainActivity.this,message,Toast.LENGTH_LONG).show();
             }
         });
-    }*/
+    }
 }
